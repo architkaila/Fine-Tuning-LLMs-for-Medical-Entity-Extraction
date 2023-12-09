01649d3d907c86f4ee787820e609e6ee65018aaf
Archit Kaila <archit.kaila@gmail.com>
2023-12-10 03:42:53 +0530

    created train & test jsons and data prepare script

--- data-preparation/combine-data.py
+++ data-preparation/combine-data.py
@@ -1,5 +1,6 @@
 import os
 import json
+import random
 
 # Replace 'your_folder_path' with the actual path to your folder containing text files
 folder_path = './data-preparation/entity_extraction_reports/'
@@ -19,13 +20,27 @@ for filename in os.listdir(folder_path):
         with open(file_path, 'r') as file:
             try:
                 json_object = json.loads(file.read())
+                
+                # Convert the 'output' field from a JSON object to a JSON string
+                for item in json_object:
+                    item['output'] = json.dumps(item['output']) 
+                
                 json_objects.extend(json_object)
             except json.JSONDecodeError:
                 print(f"Error reading file: {file_path}")
 
-# Combine all JSON objects into one JSON array
-combined_json = json.dumps(json_objects, indent=4)
+# Shuffle the JSON objects
+random.shuffle(json_objects)
 
-# Save the combined JSON to a file (optional)
-with open('./data-preparation/entity-extraction-data.json', 'w') as output_file:
-    output_file.write(combined_json)
+# Split the data into train and test
+train_data = json_objects[:700]  # First 700 objects for training
+test_data = json_objects[700:]   # Last 59 objects for testing
+
+# Write the train data to a file
+with open('./data-preparation/entity-extraction-train-data.json', 'w') as file:
+    json.dump(train_data, file, indent=4)
+
+# Write the test data to a file
+with open('./data-preparation/entity-extraction-test-data.json', 'w') as file:
+    json.dump(test_data, file, indent=4)
+
